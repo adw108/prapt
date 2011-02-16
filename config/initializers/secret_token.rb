a0da857585c64cfb59dcdee893718ea113ca63ec
Aaron D Williams <aaron@aarondwilliams.org>
2011-02-15 19:17:54 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prapt::Application.config.secret_token = 'ffe513c4555ddf9383782e073b4d8433302e7d2b8057598555fac374ff02e4978e4624581164bdb304a7ae851013516c498d75b37dec14b659949ac30c8461d0'
