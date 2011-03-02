69b7eefdc06939feca03d77205c5a4226f7f8adc
Aaron D Williams <aaron@aarondwilliams.org>
2011-03-01 19:07:00 -0800

    added meetup consumer auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,6 +5,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'A3ykppaslKIk4fLThKrbg', 'XerqdZttZasIF0cLbex3Pa8e0fduIIeAdmvORIggAA4'
   provider :openid, nil, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
 #   # provider :googleapps, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-#   # provider :meetup, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  provider :meetup, 'avjod439va6g8kuobovhv3085e', 'fjl8i36prjc0fhut1mkq540q9d'
   provider :linked_in, 'pey_oyfiL-YzrVwPIcLUdzUwj7ctBdofb2sUyuJyKpnenn8yS0yQ7ymGjXZ8455U', 'nNq0ylRnhgKcDxylKt8ez_QFRX5uV117rbyWWGyEqp-kdR-68IW6T6PyaQEVVBDz'
 end
